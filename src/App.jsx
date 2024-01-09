c3c9928e5d516ac46acf10c771a7fda2e7255bcb
Hyedenma Sey <hyedenmasey@gmail.com>
2024-01-09 22:55:13 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import "./App.css";
+
+export const App = () => {
+  return (
+    <div className="app">
+      <div className="overlap-wrapper">
+        <div className="overlap">
+          <div className="hintergrund">
+            <div className="overlap-group">
+              <div className="background-color" />
+              <img
+                className="img"
+                alt="Background color"
+                src="https://cdn.animaapp.com/projects/6436f002ef3e692942ef50ac/releases/659dba02101e6953507ffe6c/img/background-color-1.svg"
+              />
+            </div>
+          </div>
+          <img
+            className="timer"
+            alt="Timer"
+            src="https://cdn.animaapp.com/projects/6436f002ef3e692942ef50ac/releases/659dba02101e6953507ffe6c/img/timer.png"
+          />
+          <img
+            className="button-select-ship"
+            alt="Button select ship"
+            src="https://cdn.animaapp.com/projects/6436f002ef3e692942ef50ac/releases/659dba02101e6953507ffe6c/img/button-select-ship.png"
+          />
+          <div className="carousel-paging">
+            <div className="ellipse" />
+            <div className="div" />
+            <div className="ellipse-2" />
+            <div className="ellipse-3" />
+            <div className="ellipse-4" />
+          </div>
+          <img
+            className="carousel-navigation"
+            alt="Carousel navigation"
+            src="https://cdn.animaapp.com/projects/6436f002ef3e692942ef50ac/releases/659dba02101e6953507ffe6c/img/carousel-navigation.png"
+          />
+          <div className="carousel">
+            <div className="overlap-2">
+              <div className="carousel-left">
+                <div className="overlap-group-2">
+                  <div className="frame-timer" />
+                  <img
+                    className="element"
+                    alt="Element"
+                    src="https://cdn.animaapp.com/projects/6436f002ef3e692942ef50ac/releases/659dba02101e6953507ffe6c/img/8-3@2x.png"
+                  />
+                </div>
+              </div>
+              <div className="carousel-right">
+                <div className="overlap-group-2">
+                  <div className="frame-timer-2" />
+                  <img
+                    className="element"
+                    alt="Element"
+                    src="https://cdn.animaapp.com/projects/6436f002ef3e692942ef50ac/releases/659dba02101e6953507ffe6c/img/8-3@2x.png"
+                  />
+                </div>
+              </div>
+              <div className="carousel-active">
+                <div className="overlap-group-2">
+                  <div className="frame-timer-3" />
+                  <img
+                    className="element"
+                    alt="Element"
+                    src="https://cdn.animaapp.com/projects/6436f002ef3e692942ef50ac/releases/659dba02101e6953507ffe6c/img/8-3@2x.png"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            className="button-connect"
+            alt="Button connect"
+            src="https://cdn.animaapp.com/projects/6436f002ef3e692942ef50ac/releases/659dba02101e6953507ffe6c/img/button-connect-wallet.png"
+          />
+          <img
+            className="text-box"
+            alt="Text box"
+            src="https://cdn.animaapp.com/projects/6436f002ef3e692942ef50ac/releases/659dba02101e6953507ffe6c/img/text-box.png"
+          />
+          <img
+            className="punklogo"
+            alt="Punklogo"
+            src="https://cdn.animaapp.com/projects/6436f002ef3e692942ef50ac/releases/659dba02101e6953507ffe6c/img/punklogo.png"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
